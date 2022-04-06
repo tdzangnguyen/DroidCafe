4dfbd7cce66eee62273ae813f30293c40199749e
Giang <giangnguyen010801@gmail.com>
2022-04-06 21:40:45 +0700

    First Push 6/4

---
+++ app/src/main/java/com/android/exercise1week8/MainActivity.java
@@ -0,0 +1,63 @@
+package com.android.exercise1week8;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE =
+            "com.example.android.exercise1week8.extra.MESSAGE";
+    private String mOrderMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, mOrderMessage);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+        startActivity(intent);
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    public void showDonutOrder(View view) {
+        mOrderMessage = getString(R.string.donut_order_message);
+        displayToast(mOrderMessage);
+    }
+
+    public void showIceCreamOrder(View view) {
+        mOrderMessage = getString(R.string.ice_cream_order_message);
+        displayToast(mOrderMessage);
+    }
+
+    public void showFroyoOrder(View view) {
+        mOrderMessage = getString(R.string.froyo_order_message);
+        displayToast(mOrderMessage);
+    }
+
+
+}
